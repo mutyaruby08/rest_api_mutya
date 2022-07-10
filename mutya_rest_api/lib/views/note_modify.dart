87e85875223e2c42f2ba833bc864bbaf8cd40692
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2022-07-10 17:48:57 +0800

    Fetching single item using GET method part 2

--- mutya_rest_api/lib/views/note_modify.dart
+++ mutya_rest_api/lib/views/note_modify.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import '../models/note.dart';
+import '../services/notes_services.dart';
 
-class NoteModify extends StatelessWidget {
+class NoteModify extends StatefulWidget {
   final String? noteID;
-  bool get isEditing => noteID != null;
+  const NoteModify({Key? key, this.noteID}) : super(key: key);
 
-  NoteModify({this.noteID});
+  @override
+  State<NoteModify> createState() => _NoteModifyState();
+}
+
+class _NoteModifyState extends State<NoteModify> {
+  bool get isEditing => widget.noteID != null;
+
+  NotesService get notesService => GetIt.I<NotesService>();
+
+  String? errorMessage;
+  Note? note;
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _isLoading = true;
+    });
+    notesService.getNote(widget.noteID!).then((response) {
+      setState(() {
+        _isLoading = false;
+      });
+
+      if (response.error) {
+        errorMessage = response.errorMessage ?? 'An error occurred';
+      }
+      note = response.data!;
+      _titleController.text = note!.noteTitle;
+      _contentController.text = note!.noteContent;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,31 +51,44 @@ class NoteModify extends StatelessWidget {
       appBar: AppBar(title: Text(isEditing ? 'Edit note' : 'Create note')),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(hintText: 'Note title'),
-            ),
-            Container(height: 8),
-            const TextField(
-              decoration: InputDecoration(hintText: 'Note content'),
-            ),
-            Container(height: 16),
-            SizedBox(
-              width: double.infinity,
-              height: 35,
-              child: ElevatedButton(
-                child: const Text('Submit'),
-                style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor,
-                    textStyle: const TextStyle(color: Colors.white)),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
+        child: _isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Column(
+                children: <Widget>[
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(hintText: 'Note Title'),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextField(
+                    controller: _contentController,
+                    decoration: const InputDecoration(hintText: 'Note Content'),
+                  ),
+                  Container(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 35,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (isEditing) {
+                          //update note in api
+                        } else {
+                          //create note in api
+                        }
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Theme.of(context).primaryColor,
+                      ),
+                      child: const Text('Submit'),
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
       ),
     );
   }
